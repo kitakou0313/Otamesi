a6295ca5f3812518110c57279f9de714cc5299fc
kitakou0313 <kitakou0313@gmail.com>
2020-08-09 19:51:39 +0900

    make node-pty image

---
+++ plainServer/index.js
@@ -0,0 +1,18 @@
+var os = require('os');
+var pty = require('node-pty');
+
+var shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
+
+var ptyProcess = pty.spawn(shell, [], {
+    name: 'xterm-color',
+    cols: 80,
+    rows: 30,
+    cwd: process.env.HOME,
+    env: process.env
+});
+
+ptyProcess.on('data', function (data) {
+    process.stdout.write(data);
+});
+
+ptyProcess.write('echo Hello, node-pty\r');

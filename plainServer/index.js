192de5609f349f523d5148a8ca472bfae2816f53
kitakou0313 <kitakou0313@gmail.com>
2020-08-12 14:51:44 +0900

    change port number

--- plainServer/index.js
+++ plainServer/index.js
@@ -1,21 +1,21 @@
 const pty = require("node-pty");
 const Io = require("socket.io");
 
-const express = require('express');
-const app = express()
-const server = require('http').createServer(app)
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
 
 var io = new Io(server);
-io.on('connect', socket => {
+io.on("connect", (socket) => {
   var term = pty.spawn("bash", [], {
     name: "xterm-color",
     cols: 80,
-    rows: 24
+    rows: 24,
   });
 
-  term.on('data', data => socket.emit('data', data));
-  socket.on('data', data => term.write('data', data));
-  socket.on('disconnect', () => term.destroy());
+  term.on("data", (data) => socket.emit("data", data));
+  socket.on("data", (data) => term.write("data", data));
+  socket.on("disconnect", () => term.destroy());
 });
 
-server.listen(3000);
+server.listen(5000);

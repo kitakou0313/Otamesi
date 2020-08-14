eb1861b342df31fcd1119e56db730e08e37a89ae
kitakou0313 <kitakou0313@gmail.com>
2020-08-14 20:31:32 +0900

    Enable api server to make web servers

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,42 @@
 from flask import Flask, jsonify
+from kubernetes import client, config
+
+from helper import containerMaker
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
+
+config.load_incluster_config()
+apps_v1 = client.AppsV1Api()
 
 
 @app.route('/')
 def index():
     return jsonify({
         "message": "テスト!!"
+    })
+
+
+@app.route('/servers', methods=['GET'])
+def makeServer():
+
+    apps_v1 = client.AppsV1Api()
+
+    deployment = containerMaker.create_deployment_object()
+    containerMaker.create_deployment(apps_v1, deployment)
+
+    return jsonify({
+        "msg": "サーバメイキングテスト!!"
+    })
+
+
+@app.route('/servers', methods=['DELETE'])
+def deleteServer():
+
+    containerMaker.delete_deployment(apps_v1)
+
+    return jsonify({
+        "msg": "サーバデリーとテスト!!"
     })
 
 
---
+++ backend/helper/containerMaker.py
@@ -0,0 +1,63 @@
+from kubernetes import client, config
+
+DEPLOYMENT_NAME = "nginx-deployment"
+
+
+def create_deployment_object():
+    # Configureate Pod template container
+    container = client.V1Container(
+        name="nginx",
+        image="nginx:1.15.4",
+        ports=[client.V1ContainerPort(container_port=80)],
+        resources=client.V1ResourceRequirements(
+            requests={"cpu": "100m", "memory": "200Mi"},
+            limits={"cpu": "500m", "memory": "500Mi"}
+        )
+    )
+    # Create and configurate a spec section
+    template = client.V1PodTemplateSpec(
+        metadata=client.V1ObjectMeta(labels={"app": "nginx"}),
+        spec=client.V1PodSpec(containers=[container]))
+    # Create the specification of deployment
+    spec = client.V1DeploymentSpec(
+        replicas=3,
+        template=template,
+        selector={'matchLabels': {'app': 'nginx'}})
+    # Instantiate the deployment object
+    deployment = client.V1Deployment(
+        api_version="apps/v1",
+        kind="Deployment",
+        metadata=client.V1ObjectMeta(name=DEPLOYMENT_NAME),
+        spec=spec)
+
+    return deployment
+
+
+def create_deployment(api_instance, deployment):
+    # Create deployement
+    api_response = api_instance.create_namespaced_deployment(
+        body=deployment,
+        namespace="default")
+    print("Deployment created. status='%s'" % str(api_response.status))
+
+
+def update_deployment(api_instance, deployment):
+    # Update container image
+    deployment.spec.template.spec.containers[0].image = "nginx:1.16.0"
+    # Update the deployment
+    api_response = api_instance.patch_namespaced_deployment(
+        name=DEPLOYMENT_NAME,
+        namespace="default",
+        body=deployment)
+    print("Deployment updated. status='%s'" % str(api_response.status))
+
+
+def delete_deployment(api_instance):
+    # Delete deployment
+    api_response = api_instance.delete_namespaced_deployment(
+        name=DEPLOYMENT_NAME,
+        namespace="default",
+        body=client.V1DeleteOptions(
+            propagation_policy='Foreground',
+            grace_period_seconds=5))
+    print("Deployment deleted. status='%s'" % str(api_response.status))

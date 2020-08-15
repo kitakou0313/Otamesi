1ec6baa862237cdbe6d6ab43c2fbddecd7add2fd
kitakou0313 <kitakou0313@gmail.com>
2020-08-15 17:44:23 +0900

    Make artcile page

---
+++ frontend/src/components/report.vue
@@ -0,0 +1,13 @@
+<template>
+<p>テスト</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/page/article.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+<b-nav tabs fill>
+    <b-nav-item @click="currentTab='report'">Report</b-nav-item>
+    <b-nav-item @click="currentTab='front-terminal'">Terminal</b-nav-item>
+</b-nav>
+<keep-alive>
+    <component :is="currentTab" />
+</keep-alive>
+</div>
+</template>
+
+<script>
+import report from '../components/report.vue'
+import frontTerminal from '../components/frontTerminal.vue'
+
+export default {
+    components: {
+        frontTerminal,
+        report
+  },
+  data() {
+    return {
+      currentTab : "report"
+    }
+    }
+}
+</script>
+
+<style>
+
+</style>

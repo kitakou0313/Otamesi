0dbf4a15b98fd7573bd571326c973c9f6354df40
kitakou0313 <kitakou0313@gmail.com>
2020-08-15 17:01:03 +0900

    change tag of terminal from iframe to bEmbed

--- frontend/src/components/frontTerminal.vue
+++ frontend/src/components/frontTerminal.vue
@@ -1,5 +1,10 @@
 <template>
-  <iframe src="http://localhost:7681/" frameborder="0"></iframe>
+<div>
+ <b-embed
+    type="iframe"
+    src="http://localhost:7681"
+  ></b-embed>
+</div>
 </template>
 
 <script>

83871d89f2802995cdfc71d32ce839fb66b454e6
kitakou0313 <kitakou0313@gmail.com>
2020-08-15 19:25:38 +0900

    Change display style

--- frontend/src/components/frontTerminal.vue
+++ frontend/src/components/frontTerminal.vue
@@ -1,9 +1,12 @@
 <template>
 <b-card>
+   <b-card-title>Terminal of hogehoge</b-card-title>
+   <b-card-body>
  <b-embed
     type="iframe"
     src="http://localhost:7681"
   ></b-embed>
+   </b-card-body>
 </b-card>
 </template>
 
--- frontend/src/components/report.vue
+++ frontend/src/components/report.vue
@@ -1,12 +1,28 @@
 <template>
  <b-card>
-<p>テスト</p>
+     <b-card-title>Report</b-card-title>
+     <b-form >
+     <b-card-body>
+              <b-form-textarea
+              id="textarea"
+              v-model="text"
+              placeholder="Enter something..."
+              rows="10"
+    ></b-form-textarea>
+     </b-card-body>
+     <b-card-footer ><div class="text-right"><b-button type="submit">Submit</b-button></div></b-card-footer>
+     </b-form>
  </b-card>
 </template>
 
 <script>
 export default {
-    name:"report"
+    name:"report",
+    data(){
+        return{
+            text:"Test hogehoge"
+        }
+    }
 }
 </script>
 

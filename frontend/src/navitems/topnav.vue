d11fa88d229b497b67d8a8b5dbd2f9d51c02d1bf
kitakou0313 <kitakou0313@gmail.com>
2020-08-15 15:40:26 +0900

    change top nav bar color

--- frontend/src/navitems/topnav.vue
+++ frontend/src/navitems/topnav.vue
@@ -1,22 +1,13 @@
 <template>
   <div>
-  <b-navbar toggleable="lg" type="dark" variant="info">
-    <b-navbar-brand href="#">Otamesi</b-navbar-brand>
-
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
+  <b-navbar toggleable="lg" type="light" variant="light">
     <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav>
-        <b-nav-item href="#">Link</b-nav-item>
-        <b-nav-item href="#" disabled>Disabled</b-nav-item>
-      </b-navbar-nav>
-
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
         <b-nav-item-dropdown right>
           <!-- Using 'button-content' slot -->
           <template v-slot:button-content>
-            <em>User</em>
+            User
           </template>
           <b-dropdown-item href="#">Profile</b-dropdown-item>
           <b-dropdown-item href="#">Sign Out</b-dropdown-item>

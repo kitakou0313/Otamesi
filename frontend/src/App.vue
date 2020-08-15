2a4a3c42041966bd2d5731594ca1177c96047b6d
kitakou0313 <kitakou0313@gmail.com>
2020-08-15 15:08:27 +0900

    Add top nav bar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,19 @@
 <template>
 <div>
+  <topnav></topnav>
   <frontterminal></frontterminal>
 </div>
 </template>
 
 <script>
 import frontterminal from './components/frontTerminal.vue'
+import  topnav  from "./navitems/topnav.vue"
 
 export default {
   name: 'App',
   components: {
-    frontterminal
+    frontterminal,
+    topnav
   }
 }
 </script>
---
+++ frontend/src/navitems/topnav.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">Otamesi</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="#">Link</b-nav-item>
+        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template v-slot:button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

e1304e9f2192c6da7bacb74083d85607d7c2328d
kitakou0313 <kitakou0313@gmail.com>
2020-08-15 20:29:35 +0900

    Change routing settings

---
+++ frontend/src/page/articleList.vue
@@ -0,0 +1,34 @@
+<template>
+<b-card>
+   <b-card-title>List of Trainings</b-card-title>
+   <b-card-body>
+       <b-table striped hover :items="items">
+        <template v-slot:cell(index)="data">
+        {{ data.id }}
+      </template>
+        <template v-slot:cell(Title)="data">
+            <router-link :to="{name:'articlePage', params:{ id: data.id }}">
+                {{ data.Title }}
+            </router-link>
+      </template>
+       </b-table>
+   </b-card-body>
+</b-card>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            items:[
+                 { id: 40, Title: 'Test' },
+                 { id: 21, Title: 'Test2' },
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import articleList from './page/articleList'
 import articlePage from './page/article'
 
 Vue.use(Router)
@@ -8,8 +9,14 @@ Vue.use(Router)
 export default new Router({
     mode: 'history',
     routes: [{
-        path: '/article',
-        name: 'articleList',
-        component: articlePage
-    }]
+            path: '/article',
+            name: 'articleList',
+            component: articleList,
+        },
+        {
+            path: '/article/:id',
+            name: 'articlePage',
+            component: articlePage
+        }
+    ]
 })

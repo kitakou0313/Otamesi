998acf78862e6aab07da605fa63b3d1b7ed1dd49
kitakou0313 <kitakou0313@gmail.com>
2020-08-16 12:40:28 +0900

    Add API test

--- backend/restApi.py
+++ backend/restApi.py
@@ -1,15 +1,21 @@
 from kubernetes import client, config
+from helper import containerMaker
 
 
 def main():
     config.load_incluster_config()
+    apps_v1 = client.AppsV1Api()  # for server deployment
+    core_v1_api = client.CoreV1Api()  # for LB deployment
 
-    v1 = client.CoreV1Api()
-    print("Listing pods with their IPs:")
-    ret = v1.list_pod_for_all_namespaces(watch=False)
-    for i in ret.items:
-        print("%s\t%s\t%s" %
-              (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+    deployment = containerMaker.create_deployment_object()
+    response = core_v1_api.list_pod_for_all_namespaces(watch=False)
+
+    for pod in response.items:
+        if("npmdev" in pod.metadata.name):
+            print(pod.metadata.name)
+
+    containerMaker.delete_deployment(apps_v1)
+    containerMaker.delete_LoadBalancer(core_v1_api)
 
 
 if __name__ == '__main__':
